812f56ab15c59b940a6df017b810170ec933d24b
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-12-07 18:47:47 +0530

    Create pattern14.cpp

---
+++ pattern14.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print14(int n) {
+    for(int i=0; i<n; i++){
+        for(char ch = 'A'; ch <= 'A' + i; ch++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    print14(n);
+    return 0;
+}
