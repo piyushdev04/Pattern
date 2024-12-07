cf46d169e806c5d939bc844a1c4ed64769cc7ba3
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-12-07 20:53:19 +0530

    Create pattern19.cpp

---
+++ pattern19.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print19(int n) {
+    int iniS = 0;
+    for(int i=0; i<n; i++){
+        //star
+        for(int j=1; j<=n-i; j++){
+            cout << "*";
+        }
+        //space
+        for(int j=0; j<iniS; j++){
+            cout << " ";
+        }
+        //star
+        for(int j=1; j<=n-i; j++){
+            cout << "*";
+        }
+        iniS += 2;
+        cout << endl;
+    }
+
+    iniS = 8;
+    for(int i=1; i<=n; i++){
+        //star
+        for(int j=1; j<=i; j++){
+            cout << "*";
+        }
+        //space
+        for(int j=0; j<iniS; j++){
+            cout << " ";
+        }
+        //star
+        for(int j=1; j<=i; j++){
+            cout << "*";
+        }
+        iniS -= 2;
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    print19(n);
+    return 0;
+}
