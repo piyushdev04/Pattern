58d8de5869393037f18dd34f82f9f271a67719d7
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-12-08 15:23:45 +0530

    Update pattern01.cpp

--- pattern01.cpp
+++ pattern01.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void print1(int n) {
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < 4; j++){
+        for(int j = 0; j < n; j++){
             cout << "* ";
         }
         cout << endl;
